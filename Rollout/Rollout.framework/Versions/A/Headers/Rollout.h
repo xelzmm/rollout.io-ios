6ba16eb1d553d454dfa42f5fe1d7a6b64f75af2c
Sergey Ilyevsky <sergey@rollout.io>
2016-01-28 23:37:59 +0200

    version 0.14.1, build 171

--- Rollout/Rollout.framework/Versions/A/Headers/Rollout.h
+++ Rollout/Rollout.framework/Versions/A/Headers/Rollout.h
@@ -1,5 +1,5 @@
 //
-//  Rollout SDK version 0.14.0, Build 164
+//  Rollout SDK version 0.14.1, Build 171
 //
 //  Copyright (c) 2014 Rollout.io. All rights reserved.
 //
@@ -11,8 +11,12 @@
 
 +(void)setupWithKey:(NSString *)rolloutKey developmentDevice:(BOOL)developmentDevice options:(RolloutOptions*)options;
 +(void)setupWithKey:(NSString *)rolloutKey developmentDevice: (BOOL)developmentDevice;
++(void)setupWithKey:(NSString *)rolloutKey;
 +(BOOL) rolloutDisabled;
 + (void)setRolloutDisabled:(BOOL)value;
+
++(void) setupWithDebug: (BOOL) debug options:(RolloutOptions*)options __attribute__ ((deprecated));
++(void) setupWithDebug: (BOOL) debug __attribute__ ((deprecated));
 
 @end
 
--- install/xcode_ruby_helpers/remove_rollout_from_xcodeproj.rb
+++ install/xcode_ruby_helpers/remove_rollout_from_xcodeproj.rb
@@ -18,16 +18,17 @@ class RemoveRolloutFromXcodeproj
       "Rollout.io dsym upload"
     ]
     
+    greatest_xcodeproj_version = 0
     @project.targets.each do |target|
       if target.respond_to?("product_type") and target.product_type == "com.apple.product-type.application"
         files_references.each do |file_reference|
-	  begin
-            target.frameworks_build_phase.remove_file_reference file_reference
-            target.source_build_phase.remove_file_reference file_reference
-	  rescue
-	    STDERR.puts "failed to remove #{file_reference} with exception #{$!}"
-	  end
-      
+   	    begin
+          target.frameworks_build_phase.remove_file_reference file_reference
+          target.source_build_phase.remove_file_reference file_reference
+	        rescue
+	          STDERR.puts "failed to remove #{file_reference} with exception #{$!}"
+	        end
+        
           target.build_configurations.each do |build_configuration|
             framework_search_path = build_configuration.build_settings["FRAMEWORK_SEARCH_PATHS"]
             if ! framework_search_path.nil?
@@ -43,11 +44,21 @@ class RemoveRolloutFromXcodeproj
         build_phases = target.build_phases
     
         titles_of_phases_to_remove.each do |t|
-          existing_build_phase = build_phases.find { |b| b.respond_to?(:name) and b.name == t} 
-          build_phases.delete(existing_build_phase) unless existing_build_phase.nil?
+          existing_build_phase = build_phases.find { |b| b.respond_to?(:name) and b.name == t}
+          next if existing_build_phase.nil?
+
+          xcodeproj_version_match = /ROLLOUT_lastXcodeprojConfiguration=(\d+)/.match(existing_build_phase.shell_script())
+          if(not xcodeproj_version_match.nil?)
+            this_phase_xcodeproj_version = xcodeproj_version_match[1].to_i()
+            greatest_xcodeproj_version = this_phase_xcodeproj_version if this_phase_xcodeproj_version > greatest_xcodeproj_version
+          end
+
+          build_phases.delete(existing_build_phase)
         end
       end
     end
     rollout_group.remove_from_project unless rollout_group.nil?
+
+    return greatest_xcodeproj_version
   end
 end
